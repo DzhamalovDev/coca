939e1e76e81f1dbb9250ec8173bc0f27f6c7e69b
DzhamalovDev <Dzhamalov.dev@yandex.com>
2024-09-13 18:03:27 +0700

    Created testimonials section

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,10 @@ import '../assets/scss/main.scss';
 //components
 import { useTheme } from './components/theme';
 import { useBurger } from './components/burger';
+import { useInsightSlider} from './components/home/slider.js';
+import { useTestimonialsSlider } from './components/home/slider.js';
 
 useTheme();
 useBurger();
+useInsightSlider();
+useTestimonialsSlider();
