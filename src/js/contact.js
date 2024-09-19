6516967971803578eaa35d5fecab916d00648d0c
DzhamalovDev <Dzhamalov.dev@yandex.com>
2024-09-19 12:50:08 +0700

    Created contact: hero and added contact

---
+++ src/js/contact.js
@@ -0,0 +1,10 @@
+import '../assets/scss/contact.scss'
+
+
+//components
+import { useTheme } from './components/theme';
+import { useBurger } from './components/burger';
+
+
+useTheme();
+useBurger();
